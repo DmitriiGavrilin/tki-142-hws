333ceb2b41a9fbdd8aa1cb568562634d7dca026f
DmitriiGavrilin <151382168+DmitriiGavrilin@users.noreply.github.com>
2025-11-10 12:44:51 +0300

    Update task2-1.c

--- task2-1.c
+++ task2-1.c
@@ -1,29 +1,41 @@
 #include <stdio.h>
 /**
+@brief WITH_TEMP_VARIABLE - С использованием третьей переменной
+@brief WITHOUT_TEMP_VARIABLE  - Без использования третьей переменной
+*/
+enum SwapMethod {WITH_VARIABLE = 1, WITHOUT_VARIABLE};
+/**
  * @brief Точка входа в программу
  * @return Вернет 0, если программа выполнена корректно. Иначе - 1
  */
-int main() {
+int main(void) {
     int a, b, choose;
     printf("Введите значение a:");
     scanf("%d", &a);
     printf("Введите значение b:");
     scanf("%d", &b);
-    printf("1. С использованием третьей переменной\n");
-    printf("2. Без использования третьей переменной\n");
+    printf("%d. С использованием третьей переменной\n", WITH_VARIABLE);
+    printf("%d. Без использования третьей переменной\n", WITHOUT_VARIABLE);
     printf("Введите номер варианта:");
     scanf("%d", &choose);
-    if (choose == 1)
-    {
-        int c = a;
-        a = b;
-        b = c;
-    }
-    else if (choose == 2)
-    {
-        a = a + b;
-        b = a - b;
-        a = a - b;
+    switch (choose) {
+        case WITH_VARIABLE: {
+            int temp = a;
+            a = b;
+            b = temp;
+            printf("Использован метод с временной переменной\n");
+            break;
+        }
+        case WITHOUT_VARIABLE: {
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            printf("Использован арифметический метод без временной переменной\n");
+            break;
+        }
+        default:
+            printf("Ошибка: выбран неверный вариант! Допустимые значения: %d или %d\n", WITH_VARIABLE, WITHOUT_VARIABLE);
+            return 1;
     }
     printf("После обмена: a = %d, b = %d\n", a, b);
     return 0;
