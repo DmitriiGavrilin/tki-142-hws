475c0059e607aad5c01a96ce65a5540e642f5764
DmitriiGavrilin <151382168+DmitriiGavrilin@users.noreply.github.com>
2025-11-17 14:36:40 +0300

    Create task2-2.c

---
+++ task2-2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * @brief считывает значение, 
+ * введенное с клавиатуры с проверкой ввода
+ * @return считанное значение
+ */
+double getValue();
+/**
+ * @brief Точка входа в программу
+ * @return Вернет 0, если программа выполнена корректно. Иначе - 1
+ */
+int main(void)
+{
+    const double a = 2.5;
+    double x, y;
+    printf("Введите значение х:");
+    scanf("%lf", &x);
+    if (x > a)
+    {
+        double sin_x = sin(x);
+        y = x * pow(sin_x, 2);
+    }
+    else {
+        double cos_x = cos(x);
+        y = x * pow(cos_x, 2);
+    }
+    printf("При значении x: %.2f\n", x);
+    printf("Значение функции равно: %.2f\n", y);
+    return 0;
+}
+double getValue()
+{
+    double value = 0;
+    if (!scanf("%lf",&value))
+    {
+        printf("Ошибка!\n");
+        abort();
+    }
+    return value;
+}
