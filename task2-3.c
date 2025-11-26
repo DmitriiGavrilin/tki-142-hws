bf720953112da0bc906de8185c3746046da75b76
DmitriiGavrilin <151382168+DmitriiGavrilin@users.noreply.github.com>
2025-11-26 17:19:00 +0300

    Update task2-3.c

--- task2-3.c
+++ task2-3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <float.h>
 /**
  * @brief функция, определяющая расположение точек
  * @param x1 координата x точки A
@@ -48,7 +49,7 @@ int main() {
     printf("A(%.2f, %.2f)\n", x1, y1);
     printf("B(%.2f, %.2f)\n", x2, y2);
     printf("C(%.2f, %.2f)\n", x3, y3);
-    if (collinear(x1, y1, x2, y2, x3, y3) == 0) 
+    if (collinear(x1, y1, x2, y2, x3, y3) < DBL_EPSILON) 
     {
         printf("Точки лежат на одной прямой.\n");
     } 
