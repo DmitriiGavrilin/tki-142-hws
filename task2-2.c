04a969e699f725075622bb31a45bb86e187e9fb8
DmitriiGavrilin <151382168+DmitriiGavrilin@users.noreply.github.com>
2025-12-01 14:37:03 +0300

    Update task2-2.c

--- task2-2.c
+++ task2-2.c
@@ -14,17 +14,15 @@ double getValue();
 int main(void)
 {
     const double a = 2.5;
-    double y;
+    double y = 0;
     printf("Введите значение х:");
     double x = getValue();
     if (x > a)
     {
-        double sin_x = sin(x);
-        y = x * pow(sin_x, 2);
+        y = x * pow(sin(x), 2);
     }
     else {
-        double cos_x = cos(x);
-        y = x * pow(cos_x, 2);
+        y = x * pow(cos(x), 2);
     }
     printf("При значении x: %.2f\n", x);
     printf("Значение функции равно: %.2f\n", y);
